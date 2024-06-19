5b113d7e58516b9af267eb6c9e758faddce1a9ce
brushurteeth8 <adem.hameed.123@gmail.com>
2024-06-19 17:25:10 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = 3000;
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
