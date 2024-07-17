68ca86572b1a6739bc30f5b249a984b161e105da
brushurteeth 8 <adem.hameed.123@gmail.com>
2024-07-18 00:20:28 +0100

    pulling

--- admin/src/components/login/Login.jsx
+++ admin/src/components/login/Login.jsx
@@ -17,32 +17,66 @@ const Login = () => {
   };
 
   return (
-    <div className="login-container">
-      <h2>Admin Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+    // <div className="login-container">
+    //   <h2>Admin Login</h2>
+    //   <form onSubmit={handleSubmit}>
+    //     <div className="form-group">
+    //       <label htmlFor="email">Email:</label>
+    //       <input
+    //         type="email"
+    //         id="email"
+    //         value={email}
+    //         onChange={(e) => setEmail(e.target.value)}
+    //         required
+    //       />
+    //     </div>
+    //     <div className="form-group">
+    //       <label htmlFor="password">Password:</label>
+    //       <input
+    //         type="password"
+    //         id="password"
+    //         value={password}
+    //         onChange={(e) => setPassword(e.target.value)}
+    //         required
+    //       />
+    //     </div>
+    //     <button type="submit">Login</button>
+    //   </form>
+    // </div>
+    <>
+      <div className="form-container">
+        <div className="background">
+          <div className="shape" />
+          <div className="shape" />
         </div>
-        <div className="form-group">
-          <label htmlFor="password">Password:</label>
+        <form onSubmit={handleSubmit}>
+          <h3>Login Here</h3>
+          <label htmlFor="email">email</label>
+          <input
+            type="text"
+            placeholder="Email or Phone"
+            id="username"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <label htmlFor="password">Password</label>
           <input
             type="password"
+            placeholder="Password"
             id="password"
-            value={password}
             onChange={(e) => setPassword(e.target.value)}
-            required
           />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+          <button type="submit">Log In</button>
+          <div className="social">
+            <div className="go">
+              <i className="fab fa-google" /> Google
+            </div>
+            <div className="fb">
+              <i className="fab fa-facebook" /> Facebook
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
   );
 };
 
